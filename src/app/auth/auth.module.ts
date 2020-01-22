8ac3868d86630c30d46cd5fb8143af0f8fe51fa5
Lucas Teixeira <lucasteixeira.dev@gmail.com>
2020-01-22 00:06:23 -0300

    Cutomização do tema do angular material e tela de cadastro

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
